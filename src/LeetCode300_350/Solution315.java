9bc1b48d4c81c28115d1475170a9d587542a9759
刘天雅 <1037332201@qq.com>
2018-07-24 09:21:51 +0800

    找出每个数字后面比它小的数字个数
    Signed-off-by: 刘天雅 <1037332201@qq.com>

---
+++ src/LeetCode300_350/Solution315.java
@@ -0,0 +1,17 @@
+package LeetCode300_350;
+
+import java.util.List;
+
+/**
+ * @author tanya
+ * @date 2018/7/19 20:05
+ * 315. Count of Smaller Numbers After Self
+
+You are given an integer array nums and you have to return a new counts array.
+The counts array has the property where counts[i] is the number of smaller elements to the right of nums[i].
+ */
+public class Solution315 {
+    public List<Integer> countSmaller(int[] nums) {
+        return null;
+    }
+}
